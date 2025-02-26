820cf4394ace643a89ae358d59859a54d4b04645
mykola.filimonov <35329349+mykolafilimonov@users.noreply.github.com>
2025-02-26 19:53:11 +0000

    initial simple spring app

---
+++ src/test/java/org/filimonov/telegram_payment_bot/TelegramPaymentBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.filimonov.telegram_payment_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramPaymentBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
